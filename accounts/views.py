b7ad26545b66b499d4602463f07577e541eb6c54
alamincse97 <alamin.cse97@gmail.com>
2023-09-15 11:43:43 +0600

    added to registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+ 
 def register(request):
-    return render(request, 'accounts/register.html')
-
-
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            print(form.errors)
+    else:
+        form = RegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+ 
 def user_login(request):
     return render(request, 'accounts/signin.html')
