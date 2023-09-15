aeb7ed2cd89050db840d764756ba8a9daf622834
alamincse97 <alamin.cse97@gmail.com>
2023-09-15 15:57:08 +0600

    added to authentication part

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
-    # path('profile/', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('login/', views.user_login, name = 'login'),
-    # path('logout/', views.user_logout, name = 'logout'),
+    path('logout/', views.user_logout, name = 'logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
  
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('login')
         else:
             print(form.errors)
@@ -14,4 +16,19 @@ def register(request):
     return render(request, 'accounts/register.html', {'form': form})
  
 def user_login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = user_name, password = password)
+        # print(request.POST)
+        print(user)
+        login(request, user)
+        return redirect('profile')
     return render(request, 'accounts/signin.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+def profile(request):
+    return render(request, 'accounts/dashboard.html')
