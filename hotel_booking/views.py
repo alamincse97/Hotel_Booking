0633f7b16268ef5073f5498c0d328d7a3d519033
alamincse97 <alamin.cse97@gmail.com>
2023-09-18 12:56:39 +0600

    added to search functionality

--- hotel_booking/views.py
+++ hotel_booking/views.py
@@ -3,5 +3,8 @@ from hotel_pages.models import hotel
 
 def home(request):
     searchItem = request.GET.get('searchItem')
-    Hotel = hotel.objects.all()
+    if searchItem:
+        Hotel = hotel.objects.filter(Name__icontains=searchItem)
+    else:
+        Hotel = hotel.objects.all()
     return render(request, 'index.html', {'searchItem': searchItem, 'Hotels': Hotel})
