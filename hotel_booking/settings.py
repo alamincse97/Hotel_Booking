4617f32b0a803b4c93271416187abba4b3786dd1
alamincse97 <alamin.cse97@gmail.com>
2023-09-21 11:51:56 +0600

    added to static file

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
